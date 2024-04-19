250115992242eff8e82f262df380cdbdfcd587f2
successPhil <pbasti1990@gmail.com>
2024-04-19 00:03:20 -0400

    fixed cors-headers reference for domain

--- backend/pokemon_mobile/settings.py
+++ backend/pokemon_mobile/settings.py
@@ -76,7 +76,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost:5173,http://127.0.0.1:5173,pokemon.com'
+    'http://localhost:5173,http://127.0.0.1:5173,http://pokemonko.com'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
 
